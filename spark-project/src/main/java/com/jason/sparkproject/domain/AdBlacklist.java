8626f2432ca42cccfd435bd3caf05afd15b6bd4f
Jason Zhang <workmail.jason@foxmail.com>
2018-03-15 09:24:09 +0800

    Add files via upload

---
+++ spark-project/src/main/java/com/jason/sparkproject/domain/AdBlacklist.java
@@ -0,0 +1,19 @@
+package com.jason.sparkproject.domain;
+
+/**
+ * 广告黑名单
+ * @author jason
+ *
+ */
+public class AdBlacklist {
+
+	private long userid;
+
+	public long getUserid() {
+		return userid;
+	}
+	public void setUserid(long userid) {
+		this.userid = userid;
+	}
+	
+}
