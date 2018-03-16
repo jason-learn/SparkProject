b7169685e7de746920ab0eeecb80c4eed11ba45a
Jason Zhang <workmail.jason@foxmail.com>
2018-03-16 09:41:53 +0800

    Add files via upload

---
+++ spark-project/src/main/java/com/jason/sparkproject/domain/AdUserClickCount.java
@@ -0,0 +1,40 @@
+package com.jason.sparkproject.domain;
+
+/**
+ * 用户广告点击量
+ * @author jason
+ *
+ */
+public class AdUserClickCount {
+
+	private String date;
+	private long userid;
+	private long adid;
+	private long clickCount;
+	
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public long getUserid() {
+		return userid;
+	}
+	public void setUserid(long userid) {
+		this.userid = userid;
+	}
+	public long getAdid() {
+		return adid;
+	}
+	public void setAdid(long adid) {
+		this.adid = adid;
+	}
+	public long getClickCount() {
+		return clickCount;
+	}
+	public void setClickCount(long clickCount) {
+		this.clickCount = clickCount;
+	}
+	
+}
