6f97f171a26b73377e2af07da5e5f4dfa05dce35
Jason Zhang <workmail.jason@foxmail.com>
2018-03-17 11:37:31 +0800

    Add files via upload

---
+++ spark-project/src/main/java/com/jason/sparkproject/model/AdClickTrendQueryResult.java
@@ -0,0 +1,19 @@
+package com.jason.sparkproject.model;
+
+/**
+ * 广告点击趋势查询结果
+ * @author jason
+ *
+ */
+public class AdClickTrendQueryResult {
+
+	private int count;
+
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+	
+}
