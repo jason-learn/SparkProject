64391ed400457385957242607838558f19bad923
Jason Zhang <workmail.jason@foxmail.com>
2018-03-18 10:56:18 +0800

    Add files via upload

---
+++ spark-project/src/main/java/com/jason/sparkproject/dao/ITop10CategoryDAO.java
@@ -0,0 +1,14 @@
+package com.jason.sparkproject.dao;
+
+import com.jason.sparkproject.domain.Top10Category;
+
+/**
+ * top10品类DAO接口
+ * @author jason
+ *
+ */
+public interface ITop10CategoryDAO {
+
+	void insert(Top10Category category);
+	
+}
