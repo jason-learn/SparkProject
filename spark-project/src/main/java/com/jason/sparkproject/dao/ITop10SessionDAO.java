c810e6c10cf8086f696f126c1fd33307f355e234
Jason Zhang <workmail.jason@foxmail.com>
2018-03-18 10:52:18 +0800

    Add files via upload

---
+++ spark-project/src/main/java/com/jason/sparkproject/dao/ITop10SessionDAO.java
@@ -0,0 +1,14 @@
+package com.jason.sparkproject.dao;
+
+import com.jason.sparkproject.domain.Top10Session;
+
+/**
+ * top10活跃session的DAO接口
+ * @author jason
+ *
+ */
+public interface ITop10SessionDAO {
+
+	void insert(Top10Session top10Session);
+	
+}
